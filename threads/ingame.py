ef0912bc823c67bdee3821d1fa55050c5582704b
gjancock <gjancock@atomicmail.io>
2025-12-23 20:59:59 +0800

    Refactor

--- threads/ingame.py
+++ threads/ingame.py
@@ -9,47 +9,20 @@ import core.state as state
 # Initialize Logger
 logger = setup_logger()
 
-#
-def decline_vote_watcher():
-    """
-    Detects 'vote call' popup and opens it if needed.
-    """
-    while not state.STOP_EVENT.is_set():
-        try:            
-            if image_exists("vote-no.png", region=VOTE_REGION):
-                #vote_in_progress.set()
-
-                logger.info("[INFO] RED vote button spotted! Declining vote.")
-                find_and_click("vote-no.png", region=VOTE_REGION)
-                time.sleep(0.8)  # debounce
-            
-            if image_exists("vote-no-black.png", region=VOTE_REGION):
-                #vote_in_progress.set()
-
-                logger.info("[INFO] Black vote button spotted! Declining vote.")
-                find_and_click("vote-no-black.png", region=VOTE_REGION)
-                time.sleep(0.8)  # debounce
-
-        except Exception as e:
-            logger.exception(f"[THREADS ERROR] decline_vote_watcher: {e}")
-            pass
-
-        time.sleep(0.25)
-
 def vote_requester():
-    logger.info("[THREAD] vote requester started")
+    logger.info("[THREAD] Vote watcher thread started")
 
     while not state.STOP_EVENT.is_set():
         state.SCAN_VOTE_EVENT.set()
         time.sleep(0.25)
 
-    logger.info("[THREAD] vote requester stopped")
+    logger.info("[THREAD] Vote watcher thread stopped")
 
 def lobby_message_check_requester():
-    logger.info("[THREAD] message check requester started")
+    logger.info("[THREAD] Message watcher thread started")
 
     while not state.STOP_EVENT.is_set():
         state.SCAN_LOBBY_MESSAGE_EVENT.set()
         time.sleep(0.25)
 
-    logger.info("[THREAD] message check requester stopped")
+    logger.info("[THREAD] Message watcher thread stopped")
