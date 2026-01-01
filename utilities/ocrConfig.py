10c56501ef4e82fa068cf5575646424d36f19a50
gjancock <gjancock@atomicmail.io>
2026-01-01 18:06:55 +0800

    v1.3.2 Tesseract crashing issue

--- utilities/ocrConfig.py
+++ utilities/ocrConfig.py
@@ -1,14 +1,31 @@
 import pytesseract
+import os
+from utilities.common import resource_path
 
-# Explicit path (important for PyInstaller)
-pytesseract.pytesseract.tesseract_cmd = (
-    r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-)
+def ensure_tesseract_configured():
+    """
+    Ensure pytesseract is correctly configured.
+    Safe to call multiple times.
+    """
+    if pytesseract.pytesseract.tesseract_cmd != "tesseract":
+        return
+
+    pytesseract.pytesseract.tesseract_cmd = resource_path(
+        "tesseract/tesseract.exe"
+    )
+
+    # IMPORTANT: must point to tessdata directory
+    os.environ["TESSDATA_PREFIX"] = resource_path(
+        "tesseract/tessdata"
+    )
+
 
 OCR_CONFIG = (
     r"--oem 3 --psm 7 "
+    rf'--tessdata-dir "{resource_path("tesseract/tessdata")}" '
     r"-c tessedit_char_whitelist="
     r"abcdefghijklmnopqrstuvwxyz"
     r"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     r"0123456789"
+    r"_-"
 )
